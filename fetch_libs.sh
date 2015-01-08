f422c206a74141fe9a649a17408ca7a1a7374c0d
Benjamin Reed <ranger@opennms.org>
2015-01-08 15:15:37 -0500

    make sure any errors drop us out

--- fetch_libs.sh
+++ fetch_libs.sh
@@ -1,3 +1,5 @@
+set -e
+
 ARM_DOWNLOAD="https://download.01.org/crosswalk/releases/crosswalk/android/stable/8.37.189.14/arm/crosswalk-webview-8.37.189.14-arm.zip";
 X86_DOWNLOAD="https://download.01.org/crosswalk/releases/crosswalk/android/stable/8.37.189.14/x86/crosswalk-webview-8.37.189.14-x86.zip";
 
@@ -7,7 +9,7 @@ download() {
     pushd $TMPDIR > /dev/null
     echo "Fetching $1..."
     curl -# $1 -o library.zip
-    unzip -q library.zip || exit 1
+    unzip -q library.zip
     rm library.zip
     PACKAGENAME=$(ls|head -n 1)
     echo "Installing $PACKAGENAME into xwalk_core_library..."
